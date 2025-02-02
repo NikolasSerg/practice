6954ecd9e4a8b7030e5da21dcbcc5dc0c3c6978a
NikolasSerg <nikolasalon@gmail.com>
2025-02-02 20:12:42 +0200

    swagger changes

--- backend/src/app/routes/index.ts
+++ backend/src/app/routes/index.ts
@@ -7,10 +7,8 @@ import { glob } from 'glob';
 const route = Router();
 
 export function registerRoutes(router: Router) {
-  const modulePath = path.join(__dirname, '../../modules/**/*.routes.*');
-  console.log(modulePath, '********************modulePath*******************')
+  const modulePath = path.join(process.cwd(), 'src/modules/**/*.routes.*');
   const routes = glob.sync(modulePath);
-  console.log(routes, '********************routes*******************')
   logger.info('Routers: ')
   logger.info(JSON.stringify(routes))
   routes.map(route => register(route, router));
@@ -20,26 +18,6 @@ function register(routePath: string, router: Router) {
   const route = require(routePath);
   route.register(router);
 }
-
-  // const swaggerFolderPath = path.join(process.cwd(), "src/app/swagger")
-  // const swaggerFilePath = path.join(swaggerFolderPath, "swagger.json")
-
-  // console.log(process.cwd(), '/////////////////// process.cwd()///////////')
-  // console.log(swaggerFolderPath, '/////////////////// folderPath ///////////')
-
-  // if (process.env.NODE_ENV === 'development') {
-  //   if(fs.existsSync(swaggerFolderPath)) {
-  //     if(fs.existsSync(swaggerFilePath)) {
-  //       register(swaggerFilePath, route)
-  //     } else {
-  //       logger.warn('The file "swagger.json" doesn`t exist')
-  //     }
-  //   } else {
-  //     logger.warn('The folder "swagger" doesn`t exist')
-  //   }
-  // } else {
-  //   console.log('Swagger UI is disabled (not in development environment).');
-  // }
 
 registerRoutes(route)
 
--- backend/src/app/swagger/index.ts
+++ backend/src/app/swagger/index.ts
@@ -4,15 +4,14 @@ import fs from 'fs';
 import logger from '../utils/logger';
 import path from 'path';
 
-const test = process.cwd()
 
 function setupSwagger(app: Application): void {
   if (process.env.NODE_ENV === 'development') {
-    const swaggerPath = path.join(process.cwd(), 'src/app/swagger/swagger.json');
-    
+    const swaggerPath = path.join(__dirname, 'swagger.json');
     if(fs.existsSync(swaggerPath)) {
       const swaggerDocument = JSON.parse(fs.readFileSync(swaggerPath, 'utf-8'));
       app.use(`/api-docs`, swaggerUi.serve, swaggerUi.setup(swaggerDocument))
+      logger.info('Swagger was connected');
     } else {
       logger.error('swagger.json file in not found')
     }
